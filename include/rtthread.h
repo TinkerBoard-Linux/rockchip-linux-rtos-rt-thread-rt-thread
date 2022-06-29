2d97127205f8eb1714ec866559f4ea12d6167230
a1012112796 <1012112796@qq.com>
2022-06-29 14:21:21 +0800

    [enhancement] Add string information for error (#3186)
    * [enhancement]Add string information for error
    
    * Update src/kservice.c
    
    * Update src/kservice.c
    
    Co-authored-by: Man, Jianting (Meco) <920369182@qq.com>
    
    * remove %m
    
    Signed-off-by: a1012112796 <1012112796@qq.com>
    
    Co-authored-by: Meco Man <920369182@qq.com>

--- include/rtthread.h
+++ include/rtthread.h
@@ -596,6 +596,7 @@ void rt_console_set_output_hook(console_hook hook);
 rt_err_t rt_get_errno(void);
 void rt_set_errno(rt_err_t no);
 int *_rt_errno(void);
+const char *rt_strerror(rt_err_t error);
 #if !defined(RT_USING_NEWLIB) && !defined(_WIN32)
 #ifndef errno
 #define errno    *_rt_errno()
