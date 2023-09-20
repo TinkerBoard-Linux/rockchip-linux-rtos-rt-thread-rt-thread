85476019d2d9a45bf9bf422911a0991d725e1216
Cliff Chen <cliff.chen@rock-chips.com>
2023-09-20 16:43:49 +0800

    [BSP] Rockchip: RV1103: Add initial version
    Change-Id: I8e910227055d90f3cefa3d3f4f8d4168510f8bc1
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

---
+++ bsp/rockchip/rv1103/board/rv1103_evb1/iomux.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2022 Rockchip Electronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-06-09     Cliff chen   first implementation
+ */
+
+#include "rtdef.h"
+#include "iomux.h"
+#include "hal_base.h"
+
+/**
+ * @brief  Config io domian for board of rv1103_evb1
+ */
+
+void rt_hw_iodomain_config(void)
+{
+}
+
+/**
+ * @brief  Config iomux for RV1103
+ */
+void rt_hw_iomux_config(void)
+{
+    rt_hw_iodomain_config();
+
+#ifdef RT_USING_UART2
+    uart2_m0_iomux_config();
+#endif
+}
