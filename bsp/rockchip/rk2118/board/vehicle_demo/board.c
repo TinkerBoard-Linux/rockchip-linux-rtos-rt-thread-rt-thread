90fba35eb9d304ade1e9fd1d78c0842fff37740d
Jun Zeng <jun.zeng@rock-chips.com>
2024-03-01 20:44:37 +0800

    bsp: rk2118: board: add vehicle_demo for automotive terminal
    Change-Id: Ib691ecc1d9671d1592e347299e185d4dc7b2917d
    Signed-off-by: Jun Zeng <jun.zeng@rock-chips.com>

---
+++ bsp/rockchip/rk2118/board/vehicle_demo/board.c
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024-02-07     Cliff Chen   first implementation
+ */
+
+#include <rthw.h>
+#include <rtthread.h>
+
+#include "board.h"
+#include "drv_clock.h"
+#include "drv_uart.h"
+#include "drv_cache.h"
+#include "drv_heap.h"
+#include "hal_base.h"
+#include "hal_bsp.h"
+#include "iomux.h"
+
+#if defined(RT_USING_CLK_CONFIG0)
+const struct clk_init clk_inits[] =
+{
+    INIT_CLK("PLL_GPLL", PLL_GPLL, 800000000),
+    INIT_CLK("PLL_VPLL0", PLL_VPLL0, 983040000),
+    INIT_CLK("PLL_VPLL1", PLL_VPLL1, 903168000),
+    INIT_CLK("PLL_GPLL_DIV", PLL_GPLL_DIV, 200000000),
+    INIT_CLK("PLL_VPLL0_DIV", PLL_VPLL0_DIV, 122880000),
+    INIT_CLK("PLL_VPLL1_DIV", PLL_VPLL1_DIV, 112896000),
+    INIT_CLK("CLK_DSP0_SRC", CLK_DSP0_SRC, 400000000),
+    INIT_CLK("CLK_DSP0", CLK_DSP0, 700000000),
+    INIT_CLK("CLK_DSP1", CLK_DSP1, 491520000),
+    INIT_CLK("CLK_DSP2", CLK_DSP2, 491520000),
+    INIT_CLK("ACLK_NPU", ACLK_NPU, 400000000),
+    INIT_CLK("HCLK_NPU", HCLK_NPU, 150000000),
+    INIT_CLK("CLK_STARSE0", CLK_STARSE0, 400000000),
+    INIT_CLK("CLK_STARSE1", CLK_STARSE1, 400000000),
+    INIT_CLK("ACLK_BUS", ACLK_BUS, 300000000),
+    INIT_CLK("HCLK_BUS", HCLK_BUS, 150000000),
+    INIT_CLK("PCLK_BUS", PCLK_BUS, 150000000),
+    INIT_CLK("ACLK_HSPERI", ACLK_HSPERI, 150000000),
+    INIT_CLK("ACLK_PERIB", ACLK_PERIB, 150000000),
+    INIT_CLK("HCLK_PERIB", HCLK_PERIB, 150000000),
+    INIT_CLK("CLK_INT_VOICE0", CLK_INT_VOICE0, 49152000),
+    INIT_CLK("CLK_INT_VOICE1", CLK_INT_VOICE1, 45158400),
+    INIT_CLK("CLK_INT_VOICE2", CLK_INT_VOICE2, 98304000),
+    INIT_CLK("CLK_FRAC_UART0", CLK_FRAC_UART0, 64000000),
+    INIT_CLK("CLK_FRAC_UART1", CLK_FRAC_UART1, 48000000),
+    INIT_CLK("CLK_FRAC_VOICE0", CLK_FRAC_VOICE0, 24576000),
+    INIT_CLK("CLK_FRAC_VOICE1", CLK_FRAC_VOICE1, 22579200),
+    INIT_CLK("CLK_FRAC_COMMON0", CLK_FRAC_COMMON0, 12288000),
+    INIT_CLK("CLK_FRAC_COMMON1", CLK_FRAC_COMMON1, 11289600),
+    INIT_CLK("CLK_FRAC_COMMON2", CLK_FRAC_COMMON2, 8192000),
+    INIT_CLK("PCLK_PMU", PCLK_PMU, 100000000),
+    INIT_CLK("CLK_32K_FRAC", CLK_32K_FRAC, 32768),
+    INIT_CLK("CLK_MAC_OUT", CLK_MAC_OUT, 50000000),
+    /* Audio */
+    INIT_CLK("MCLK_PDM", MCLK_PDM, 100000000),
+    INIT_CLK("CLKOUT_PDM", CLKOUT_PDM, 3072000),
+    INIT_CLK("MCLK_SPDIFTX", MCLK_SPDIFTX, 6144000),
+    INIT_CLK("MCLK_OUT_SAI0", MCLK_OUT_SAI0, 24576000),
+    INIT_CLK("MCLK_OUT_SAI1", MCLK_OUT_SAI1, 24576000),
+    INIT_CLK("MCLK_OUT_SAI2", MCLK_OUT_SAI2, 24576000),
+    INIT_CLK("MCLK_OUT_SAI3", MCLK_OUT_SAI3, 24576000),
+    INIT_CLK("MCLK_OUT_SAI4", MCLK_OUT_SAI4, 24576000),
+    INIT_CLK("MCLK_OUT_SAI5", MCLK_OUT_SAI5, 24576000),
+    INIT_CLK("MCLK_OUT_SAI6", MCLK_OUT_SAI6, 24576000),
+    INIT_CLK("MCLK_OUT_SAI7", MCLK_OUT_SAI7, 24576000),
+    INIT_CLK("CLK_TSADC", CLK_TSADC, 1200000),
+    INIT_CLK("CLK_TSADC_TSEN", CLK_TSADC_TSEN, 12000000),
+    INIT_CLK("SCLK_SAI0", SCLK_SAI0, 24576000),
+    INIT_CLK("SCLK_SAI1", SCLK_SAI1, 24576000),
+    INIT_CLK("SCLK_SAI2", SCLK_SAI2, 24576000),
+    INIT_CLK("SCLK_SAI3", SCLK_SAI3, 24576000),
+    INIT_CLK("SCLK_SAI4", SCLK_SAI4, 24576000),
+    INIT_CLK("SCLK_SAI5", SCLK_SAI5, 24576000),
+    INIT_CLK("SCLK_SAI6", SCLK_SAI6, 24576000),
+    INIT_CLK("SCLK_SAI7", SCLK_SAI7, 24576000),
+    { /* sentinel */ },
+};
+#endif
+
+#if defined(RT_USING_UART0)
+const struct uart_board g_uart0_board =
+{
+    .baud_rate = UART_BR_1500000,
+    .dev_flag = ROCKCHIP_UART_SUPPORT_FLAG_DEFAULT,
+    .bufer_size = RT_SERIAL_RB_BUFSZ,
+    .name = "uart0",
+};
+#endif /* RT_USING_UART0 */
+
+#if defined(RT_USING_UART2)
+const struct uart_board g_uart2_board =
+{
+    .baud_rate = UART_BR_1500000,
+    .dev_flag = ROCKCHIP_UART_SUPPORT_FLAG_DEFAULT,
+    .bufer_size = RT_SERIAL_RB_BUFSZ,
+    .name = "uart2",
+};
+#endif /* RT_USING_UART2 */
+
+#ifdef RT_USING_UNCACHE_HEAP
+extern const rt_uint32_t __uncache_heap_start__[];
+extern const rt_uint32_t __uncache_heap_end__[];
+#endif
+#ifdef __ARMCC_VERSION
+extern const rt_uint32_t Image$$ARM_LIB_HEAP$$Limit[];
+extern const rt_uint32_t Image$$ARM_LIB_STACK$$Base[];
+#define HEAP_START       Image$$ARM_LIB_HEAP$$Limit
+#define HEAP_END         Image$$ARM_LIB_STACK$$Base
+#else
+extern const rt_uint32_t __heap_begin__[];
+extern const rt_uint32_t __heap_end__[];
+#define HEAP_START       (__heap_begin__)
+#define HEAP_END         (__heap_end__)
+#endif
+
+/**
+ * This function will initial Pisces board.
+ */
+void rt_hw_board_init()
+{
+    /* mpu init */
+    mpu_init();
+
+    /* HAL_Init */
+    HAL_Init();
+
+    /* spinlock init */
+    spinlock_init();
+
+    /* hal bsp init */
+    BSP_Init();
+
+    /* System tick init */
+    rt_hw_interrupt_install(SysTick_IRQn, systick_isr, RT_NULL, "tick");
+    HAL_SetTickFreq(1000 / RT_TICK_PER_SECOND);
+    HAL_SYSTICK_Init();
+
+#ifdef RT_USING_PIN
+    rt_hw_iomux_config();
+#endif
+
+    rt_system_heap_init((void *)HEAP_START, (void *)HEAP_END);
+#ifdef RT_USING_UNCACHE_HEAP
+    rt_uncache_heap_init((void *)__uncache_heap_start__, (void *)__uncache_heap_end__);
+#endif
+
+    /* Initial usart deriver, and set console device */
+    rt_hw_usart_init();
+
+    clk_init(clk_inits, true);
+
+    /* Update system core clock after clk_init */
+    SystemCoreClockUpdate();
+
+#ifdef RT_USING_CONSOLE
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
+
+#ifdef RT_USING_SWO_PRINTF
+    rt_console_set_output_hook(swo_console_hook);
+#endif
+
+    /* Call components board initial (use INIT_BOARD_EXPORT()) */
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif
+}
---
+++ bsp/rockchip/rk2118/board/vehicle_demo/iomux.c
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2024 Rockchip Electronics Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2024-02-07     Cliff Chen   first implementation
+ */
+
+/** @addtogroup RKBSP_Board_Driver
+ *  @{
+ */
+
+/** @addtogroup IOMUX
+ *  @{
+ */
+
+/** @defgroup How_To_Use How To Use
+ *  @{
+ @verbatim
+
+ ==============================================================================
+                    #### How to use ####
+ ==============================================================================
+ This file provide IOMUX for board, it will be invoked when board initialization.
+
+ @endverbatim
+ @} */
+#include "rtdef.h"
+#include "iomux.h"
+#include "hal_base.h"
+
+/********************* Private MACRO Definition ******************************/
+/** @defgroup IOMUX_Private_Macro Private Macro
+ *  @{
+ */
+
+/** @} */  // IOMUX_Private_Macro
+
+/********************* Private Structure Definition **************************/
+/** @defgroup IOMUX_Private_Structure Private Structure
+ *  @{
+ */
+
+/** @} */  // IOMUX_Private_Structure
+
+/********************* Private Variable Definition ***************************/
+/** @defgroup IOMUX_Private_Variable Private Variable
+ *  @{
+ */
+
+/** @} */  // IOMUX_Private_Variable
+
+/********************* Private Function Definition ***************************/
+/** @defgroup IOMUX_Private_Function Private Function
+ *  @{
+ */
+
+/** @} */  // IOMUX_Private_Function
+
+/********************* Public Function Definition ****************************/
+
+/**
+ * @brief  Config iomux for SAI0
+ */
+void sai0_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A0,   // SAI0_TDM_SCLK
+                        RMIO_SAI0_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A1,   // SAI0_TDM_LRCK
+                        RMIO_SAI0_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A2,   // SAI0_TDM_LRCKxN_0
+                        RMIO_SAI0_LRCKXN_0);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A3,   // SAI0_TDM_SDO0
+                        RMIO_SAI0_SDO0);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A4,   // SAI0_TDM_SDO1
+                        RMIO_SAI0_SDO1);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A5,   // SAI0_TDM_SDI0
+                        RMIO_SAI0_SDI0);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A6,   // SAI0_TDM_SDI1
+                        RMIO_SAI0_SDI1);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_A7,   // SAI0_TDM_SDI2
+                        RMIO_SAI0_SDI2);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B0,   // SAI0_TDM_LRCKxN_1
+                        RMIO_SAI0_LRCKXN_1);
+}
+
+/**
+ * @brief  Config iomux for SAI1
+ */
+void sai1_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B2,   // SAI1_TDM_SCLK
+                        RMIO_SAI1_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B3,   // SAI1_TDM_LRCK
+                        RMIO_SAI1_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B4,   // SAI1_TDM_SDI0
+                        RMIO_SAI1_SDI0);
+}
+
+/**
+ * @brief  Config iomux for SAI2
+ */
+void sai2_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B5,   // SAI2_TDM_SCLK
+                        RMIO_SAI2_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B6,   // SAI2_TDM_LRCK
+                        RMIO_SAI2_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_B7,   // SAI2_TDM_SDI0
+                        RMIO_SAI2_SDI0);
+}
+
+/**
+ * @brief  Config iomux for SAI4
+ */
+void sai4_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C4,   // SAI4_TDM_SCLK
+                        RMIO_SAI4_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C3,   // SAI4_TDM_LRCK
+                        RMIO_SAI4_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C5,   // SAI4_TDM_SDI0
+                        RMIO_SAI4_SDI0);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C6,   // SAI4_TDM_SDO0
+                        RMIO_SAI4_SDO0);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C7,   // SAI4_TDM_SDO1
+                        RMIO_SAI4_SDO1);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,    // SAI4_TDM_MCLK
+                        GPIO_PIN_D3, RMIO_SAI4_MCLK);
+}
+
+/**
+ * @brief  Config iomux for SAI5
+ */
+void sai5_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C0,   // SAI5_TDM_SCLK
+                        RMIO_SAI5_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C1,   // SAI5_TDM_LRCK
+                        RMIO_SAI5_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_C2,   // SAI5_TDM_SDI0
+                        RMIO_SAI5_SDI0);
+}
+
+/**
+ * @brief  Config iomux for SAI6
+ */
+void sai6_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_D0,   // SAI6_TDM_SCLK
+                        RMIO_SAI6_SCLK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_D1,   // SAI6_TDM_LRCK
+                        RMIO_SAI6_LRCK);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK4,
+                        GPIO_PIN_D2,   // SAI6_TDM_SDI0
+                        RMIO_SAI6_SDI0);
+}
+
+/**
+ * @brief  Config iomux for UART2
+ */
+void uart2_iomux_config(void)
+{
+    HAL_PINCTRL_SetRMIO(GPIO_BANK3,
+                        GPIO_PIN_B5,   // UART2_TX_AUDIO_DEBUG
+                        RMIO_UART2_TX_RM1);
+
+    HAL_PINCTRL_SetRMIO(GPIO_BANK3,
+                        GPIO_PIN_B6,   // UART2_RX_AUDIO_DEBUG
+                        RMIO_UART2_RX_RM1);
+}
+
+/** @defgroup IOMUX_Public_Functions Public Functions
+ *  @{
+ */
+
+void rt_hw_iomux_config(void)
+{
+    sai_mclkout_config_all();
+    dsp_jtag_iomux_config();
+    mcu_jtag_m0_iomux_config();
+    spi2_iomux_config();
+    i2c2_iomux_config();
+    uart0_iomux_config();
+    uart2_iomux_config();
+    spi0_m1_iomux_config();
+    sai0_iomux_config();
+    sai1_iomux_config();
+    sai2_iomux_config();
+    sai4_iomux_config();
+    sai5_iomux_config();
+    sai6_iomux_config();
+}
+
+/** @} */  // IOMUX_Public_Functions
+
+/** @} */  // IOMUX
+
+/** @} */  // RKBSP_Board_Driver
