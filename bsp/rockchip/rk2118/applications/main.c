958dbe1b5c9fe258984a93a25b9639a708ca4ce7
Cliff Chen <cliff.chen@rock-chips.com>
2024-03-08 11:37:47 +0000

    [BSP] Rockchip: RK2118: Add memory bandwidth test
    Change-Id: I5c463aa9eea6dbafa3d09e68b45cf018b379c989
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

--- bsp/rockchip/rk2118/applications/main.c
+++ bsp/rockchip/rk2118/applications/main.c
@@ -49,6 +49,7 @@ int main(void)
     //test_reg_read();
 
     __asm volatile("vldr d7, %0" : : "m"(g_double) :);
+    HAL_DelayMs(100);
     //tfm_ns_interface_init();
     //psa_framework_version();
     return 0;
@@ -84,4 +85,169 @@ void cpu_stress(int argc, char **argv)
 #ifdef RT_USING_FINSH
 #include <finsh.h>
 MSH_CMD_EXPORT(cpu_stress, cpu stress test);
+#endif
+
+void mem_bw_read(void *src, uint32_t size, uint32_t loop)
+{
+    uint32_t i = 0;
+    uint64_t time1, time2;
+    float time_us, bw;
+
+    time1 = HAL_GetSysTimerCount();
+    while (i++ < loop)
+    {
+        asm volatile(
+            "   stmdb   sp!, {r2, r3, r4, r5, r6, r7, r8} \n"
+            "   mov  r7, %[size]            \n"
+            "   mov  r2, %[addr]            \n"
+            "0:                             \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   ldmia r2!, {r3-r6}          \n"
+            "   add r8, r8, #128            \n"
+            "   cmp r8, r7                  \n"
+            "   blo 0b                      \n"
+            "   ldmia   sp!, {r2, r3, r4, r5, r6, r7, r8} \n"
+            :
+            : [addr] "r"(src), [size] "r"(size)
+            : "cc", "memory"
+        );
+    }
+    time2 = HAL_GetSysTimerCount();
+    if (time2 > time1)
+        time_us = ((time2 - time1) * 1000000.0) / PLL_INPUT_OSC_RATE;
+    else
+        time_us = ((time1 - time2) * 1000000.0) / PLL_INPUT_OSC_RATE;
+    bw = (size * loop) / time_us;   /* MB/s */
+    rt_kprintf("mem_bw_read: %d MB/s\n", (uint32_t)bw);
+}
+
+void mem_bw_write(void *dst, uint32_t size, uint32_t loop)
+{
+    uint32_t i = 0;
+    uint64_t time1, time2;
+    float time_us, bw;
+
+    time1 = HAL_GetSysTimerCount();
+    while (i++ < loop)
+    {
+        asm volatile(
+            "   stmdb   sp!, {r2, r7, r8}   \n"
+            "   mov  r7, %[size]            \n"
+            "   mov  r2, %[addr]            \n"
+            "   mov  r8, #0                 \n"
+            "0:                             \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   stmia r2!, {r3-r6}          \n"
+            "   add r8, r8, #128            \n"
+            "   cmp r8, r7                  \n"
+            "   blo 0b                      \n"
+            "   ldmia sp!, {r2, r7, r8}     \n"
+            :
+            : [addr] "r"(dst), [size] "r"(size)
+            : "cc", "memory"
+        );
+    }
+    time2 = HAL_GetSysTimerCount();
+    if (time2 > time1)
+        time_us = ((time2 - time1) * 1000000.0) / PLL_INPUT_OSC_RATE;
+    else
+        time_us = ((time1 - time2) * 1000000.0) / PLL_INPUT_OSC_RATE;
+    bw = (size * loop) / time_us;   /* MB/s */
+    rt_kprintf("mem_bw_write: %d MB/s\n", (uint32_t)bw);
+}
+
+void mem_bw_copy(void *src, void *dst, uint32_t size, uint32_t loop)
+{
+    uint32_t i = 0;
+    uint64_t time1, time2;
+    float time_us, bw;
+
+    time1 = HAL_GetSysTimerCount();
+    while (i++ < loop)
+    {
+        asm volatile(
+            "   stmdb   sp!, {r0-r8}        \n"
+            "   mov  r4, %[src]             \n"
+            "   mov  r5, %[dst]             \n"
+            "   mov  r6, %[size]            \n"
+            "   mov  r8, #0                 \n"
+            "0:                             \n"
+            "   ldmia r4!, {r0-r3}          \n"
+            "   stmia r5!, {r0-r3}          \n"
+            "   ldmia r4!, {r0-r3}          \n"
+            "   stmia r5!, {r0-r3}          \n"
+            "   ldmia r4!, {r0-r3}          \n"
+            "   stmia r5!, {r0-r3}          \n"
+            "   ldmia r4!, {r0-r3}          \n"
+            "   stmia r5!, {r0-r3}          \n"
+            "   add r8, r8, #128            \n"
+            "   cmp r8, r6                  \n"
+            "   blo 0b                      \n"
+            "   ldmia sp!, {r0-r8}          \n"
+            : [dst] "+r"(dst)
+            : [src] "r"(src), [size] "r"(size)
+            : "cc", "memory"
+        );
+    }
+    time2 = HAL_GetSysTimerCount();
+    time_us = ((time2 - time1) * 1000000.0) / PLL_INPUT_OSC_RATE;
+    bw = (size * loop) / time_us;   /* MB/s */
+    rt_kprintf("mem_bw_copy: %d MB/s\n", (uint32_t)bw);
+}
+
+void mem_bw_test(int argc, char **argv)
+{
+    void *src, *dst;
+    uint32_t size, loop;
+    char op;
+
+    if (argc != 6)
+    {
+        rt_kprintf("Usage: mem_bw_test op src dst size loop, op=r,w,c\n");
+        return;
+    }
+    op = argv[1][0];
+    src = (void *)strtoul(argv[2], NULL, 16);
+    dst = (void *)strtoul(argv[3], NULL, 16);
+    size = (uint32_t)atoi(argv[4]);
+    loop = (uint32_t)atoi(argv[5]);
+
+    rt_kprintf("mem_bw_test: op=%c, src=0x%x, dst=0x%x, size=%d, loop=%d\n",
+               op, (uint32_t)src, (uint32_t)dst, size, loop);
+
+    if (size % 128 != 0)
+    {
+        rt_kprintf("size must be 128 bytes aligned\n");
+        return;
+    }
+
+    switch (op)
+    {
+    case 'r':
+        mem_bw_read(src, size, loop);
+        break;
+    case 'w':
+        mem_bw_write(dst, size, loop);
+        break;
+    case 'c':
+        mem_bw_copy(src, dst, size, loop);
+        break;
+    }
+}
+
+#ifdef RT_USING_FINSH
+#include <finsh.h>
+MSH_CMD_EXPORT(mem_bw_test, mem bandwidth test);
 #endif
