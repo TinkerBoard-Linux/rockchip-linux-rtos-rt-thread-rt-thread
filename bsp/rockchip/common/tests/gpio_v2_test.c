4ab7c1f937da9d757af5ad494304601f18758d04
zain.wang <wzz@rock-chips.com>
2024-03-22 15:55:53 +0800

    [BSP]: rockchip: common: tests: gpio_v2: add "atgpio" command
    "atgpio" used two pin to test gpio input, output,
    and irq functions.
    
    Example: atgpio gpio4 26 gpio4 27
    
    Signed-off-by: zain.wang <wzz@rock-chips.com>
    Change-Id: Ief174a11deac8faf9bb18a0ca8294dcdb12b570e

--- bsp/rockchip/common/tests/gpio_v2_test.c
+++ bsp/rockchip/common/tests/gpio_v2_test.c
@@ -31,6 +31,12 @@ struct TGPIO_INFO
     uint32_t bank;
 };
 
+struct tgpio_pin
+{
+    uint32_t bank;
+    uint32_t pin;
+};
+
 struct TGPIO_INFO gpios[] =
 {
     { "gpio0", GPIO0, GPIO_BANK0 },
@@ -86,8 +92,8 @@ static uint32_t irq_mode_table[5] =
 static rt_device_t pin_dev = RT_NULL;
 #endif
 static struct rt_device_pin_mode   pin_mode;
-static struct rt_device_pin_status pin_status;
-static uint32_t isr_flag = 0;
+static struct rt_device_pin_status g_pin_status;
+static volatile uint32_t isr_flag = 0;
 
 static void irq_callback(void *args)
 {
@@ -105,10 +111,13 @@ static void irq_callback(void *args)
     rt_kprintf("isr: gpio%d pin%d input level %d\n", PIN_BANK(pstatus->pin), PIN_NUM(pstatus->pin), pstatus->status);
 }
 
-static void gpio_test(uint32_t bank, uint32_t pin, uint32_t dir, uint32_t parm1, uint32_t parm2)
+#define FLAG_INPUT_WAIT_ISR     (1 << 0)
+static void gpio_test(uint32_t bank, uint32_t pin, uint32_t dir,
+                      uint32_t parm1, uint32_t parm2, uint32_t flag)
 {
     rt_err_t ret;
     uint32_t i;
+    struct rt_device_pin_status pin_status;
 
     rt_kprintf("bank = %d, pin = %d, dir = %d, parm1 = %d, parm2 = %d\n",
                bank, pin, dir, parm1, parm2);
@@ -154,14 +163,18 @@ static void gpio_test(uint32_t bank, uint32_t pin, uint32_t dir, uint32_t parm1,
 #else
         rt_pin_mode(pin_mode.pin, pin_mode.mode);
 #endif
-        rt_pin_attach_irq(pin_mode.pin, irq_mode_table[parm2], irq_callback, (void *)&pin_status);
+        memcpy(&g_pin_status, &pin_status, sizeof(pin_status));
+        rt_pin_attach_irq(pin_mode.pin, irq_mode_table[parm2], irq_callback, (void *)&g_pin_status);
 
         rt_pin_irq_enable(pin_mode.pin, PIN_IRQ_ENABLE);
 
+        isr_flag = 0;
         rt_kprintf("wait for gpio%d pin%d input...\n", bank, pin);
 
+        if (!(flag & FLAG_INPUT_WAIT_ISR))
+            return;
+
         //wait isr
-        isr_flag = 0;
         for (i = 0; i < 60; i++)
         {
             HAL_DelayMs(1000);
@@ -179,81 +192,97 @@ static void gpio_test(uint32_t bank, uint32_t pin, uint32_t dir, uint32_t parm1,
     }
 }
 
+static int tgpio_get_pin(struct tgpio_pin *tpin, int argc, char **argv)
+{
+    int i;
+    char *pstr;
+
+    /* Get pin bank */
+    for (i = 0; i < GPIO_BANK_NUM; i++)
+    {
+        if (!strcmp(gpios[i].desc, argv[0]))
+        {
+            tpin->bank = gpios[i].bank;
+            break;
+        }
+    }
+
+    if (i >= GPIO_BANK_NUM)
+        return 0;
+
+    /* Get pin num */
+    tpin->pin = strtol(argv[1], &pstr, 10);
+    if ((*pstr != 0) || (tpin->pin >= 32))
+        return 0;
+
+    return 2;
+}
+
 static void tgpio_main(int argc, char **argv)
 {
+    uint32_t index = 1;
     char *pstr;
-    uint32_t i;
-    uint32_t bank  = (uint32_t) -1;
-    uint32_t pin   = (uint32_t) -1;
+    struct tgpio_pin tpin;
     uint32_t dir   = (uint32_t) -1;
     uint32_t parm1 = (uint32_t) -1;
     uint32_t parm2 = (uint32_t) -1;
+    int ret;
 
     if (argc > 5)
     {
-        for (i = 0; i < GPIO_BANK_NUM; i++)
+        ret = tgpio_get_pin(&tpin, argc - index, &argv[index]);
+        if (!ret)
+            goto usage;
+
+        index = index + ret;
+
+        // Get dir
+        if (!strcmp("-o", argv[index]))
         {
-            if (!strcmp(gpios[i].desc, argv[1]))
+            dir = 1;
+            index++;
+
+            // Get parm1: output level (0~1)
+            parm1 = strtol(argv[index], &pstr, 10);
+            if ((*pstr != 0)  || (parm1 > 1))
+                goto usage;
+
+            index++;
+            // Get param2: output driven (0~3)
+            parm2 = strtol(argv[index], &pstr, 10);
+            if ((*pstr != 0)  || (parm2 > 3))
             {
-                bank = gpios[i].bank;
-                break;
+                goto usage;
             }
         }
-        if (i < GPIO_BANK_NUM)
+        else if (!strcmp("-i", argv[3]))
         {
-            // Get pin num
-            pin  = strtol(argv[2], &pstr, 10);
-            if ((*pstr != 0) || (pin >= 32))
+            dir = 0;
+            index++;
+
+            // Get parm1: input pull mode (0~2)
+            parm1 = strtol(argv[index], &pstr, 10);
+            if ((*pstr != 0)  || (parm1 > 2))
             {
                 goto usage;
             }
 
-            // Get dir
-            if (!strcmp("-o", argv[3]))
-            {
-                dir = 1;
-
-                // Get parm1: output level (0~1)
-                parm1  = strtol(argv[4], &pstr, 10);
-                if ((*pstr != 0)  || (parm1 > 1))
-                {
-                    goto usage;
-                }
-
-                // Get param2: output driven (0~3)
-                parm2  = strtol(argv[5], &pstr, 10);
-                if ((*pstr != 0)  || (parm1 > 3))
-                {
-                    goto usage;
-                }
-            }
-            else if (!strcmp("-i", argv[3]))
-            {
-                dir = 0;
-
-                // Get parm1: input pull mode (0~2)
-                parm1  = strtol(argv[4], &pstr, 10);
-                if ((*pstr != 0)  || (parm1 > 2))
-                {
-                    goto usage;
-                }
-
-                // Get param2: input irq mode (0~4)
-                parm2  = strtol(argv[5], &pstr, 10);
-                if ((*pstr != 0)  || (parm1 > 4))
-                {
-                    goto usage;
-                }
-            }
-            else
+            index++;
+            // Get param2: input irq mode (0~4)
+            parm2 = strtol(argv[index], &pstr, 10);
+            if ((*pstr != 0)  || (parm2 > 4))
             {
                 goto usage;
             }
-
-            gpio_test(bank, pin, dir, parm1, parm2);
-
-            return;
         }
+        else
+        {
+            goto usage;
+        }
+
+        gpio_test(tpin.bank, tpin.pin, dir, parm1, parm2, FLAG_INPUT_WAIT_ISR);
+
+        return;
     }
 
 usage:
@@ -264,13 +293,101 @@ usage:
     rt_kprintf("  param1:  output level     (0:low 1:high)\n");
     rt_kprintf("           input pull mode  (0:normal 1:up 2:down)\n");
     rt_kprintf("  param2:  output driving level (0 ~ 3)\n");
-    rt_kprintf("           input irq mode   (0:rising 1:falling 2:edge 4:low 5:high)\n\n");
+    rt_kprintf("           input irq mode   (0:rising 1:falling 2:edge 3:high 4:low)\n\n");
     rt_kprintf("example(output): tgpio gpio0 24 -o 1 2\n");
     rt_kprintf("example(input):  tgpio gpio0 24 -i 1 0\n\n");
+}
+
+static int atgpio_test(struct tgpio_pin *pin1, struct tgpio_pin *pin2)
+{
+    isr_flag = 0;
+    /* 1: output high */
+    gpio_test(pin1->bank, pin1->pin, 1, 1, 2, 0);
+    /* 2: input, no pullup, falling irq */
+    gpio_test(pin2->bank, pin2->pin, 0, 0, 1, 0);
+    /* 1: output low */
+    HAL_DelayMs(25);
+    gpio_test(pin1->bank, pin1->pin, 1, 0, 2, 0);
+    HAL_DelayMs(25);
+    if (!isr_flag)
+    {
+        rt_kprintf("Can't get gpio%d_%d isr (param: falling, no pullup)\n", pin2->bank, pin2->pin);
+        return -1;
+    }
+
+    /* 2: input, no pullup, rising irq */
+    gpio_test(pin2->bank, pin2->pin, 0, 0, 0, 0);
+    HAL_DelayMs(25);
+    /* 1: output high */
+    gpio_test(pin1->bank, pin1->pin, 1, 1, 2, 0);
+    HAL_DelayMs(25);
+    if (!isr_flag)
+    {
+        rt_kprintf("Can't get gpio%d_%d isr (param: rising, no pullup)\n", pin2->bank, pin2->pin);
+        return -1;
+    }
+
+    /* 2: input, no pullup, low irq */
+    gpio_test(pin2->bank, pin2->pin, 0, 0, 4, 0);
+    HAL_DelayMs(25);
+    /* 1: output low */
+    gpio_test(pin1->bank, pin1->pin, 1, 0, 2, 0);
+    HAL_DelayMs(25);
+    if (!isr_flag)
+    {
+        rt_kprintf("Can't get gpio%d_%d isr (param: low, no pullup)\n", pin2->bank, pin2->pin);
+        return -1;
+    }
+
+    /* 2: input, no pullup, high irq */
+    gpio_test(pin2->bank, pin2->pin, 0, 0, 3, 0);
+    HAL_DelayMs(25);
+    /* 1: output high */
+    gpio_test(pin1->bank, pin1->pin, 1, 1, 2, 0);
+    HAL_DelayMs(25);
+    if (!isr_flag)
+    {
+        rt_kprintf("Can't get gpio%d_%d isr (param: high, no pullup)\n", pin2->bank, pin2->pin);
+        return -1;
+    }
+
+    return 0;
+}
+
+static int atgpio_main(int argc, char **argv)
+{
+    uint32_t index = 1;
+    struct tgpio_pin tpin[2];
+    int ret;
+
+    if (argc > 4)
+    {
+        ret = tgpio_get_pin(&tpin[0], argc - index, &argv[index]);
+        if (!ret)
+            goto usage;
+        index = index + ret;
+        ret = tgpio_get_pin(&tpin[1], argc - index, &argv[index]);
+        if (!ret)
+            goto usage;
+        ret = atgpio_test(&tpin[0], &tpin[1]);
+        if (ret)
+            return ret;
+        return atgpio_test(&tpin[1], &tpin[0]);
+    }
+
+usage:
+    rt_kprintf("atgpio: auto test gpio feature: output, input, interrupt\n\n");
+    rt_kprintf("usage: atgpio <group_1> <pin_num_1> <group_2> <pin_num_2>\n");
+    rt_kprintf("  group:   group            (gpio0 ~ 4)\n");
+    rt_kprintf("  pin_num: pin number       (0 ~ 31)\n");
+    rt_kprintf("example: tgpio gpio0 24 gpio0 25\n");
+
+    return -1;
 }
 
 #ifdef RT_USING_FINSH
 #include <finsh.h>
 MSH_CMD_EXPORT_ALIAS(tgpio_main, tgpio, gpio test cmd);
+MSH_CMD_EXPORT_ALIAS(atgpio_main, atgpio, gpio auto test cmd);
 #endif
 #endif
