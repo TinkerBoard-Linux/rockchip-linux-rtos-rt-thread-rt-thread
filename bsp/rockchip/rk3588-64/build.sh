1a952d4a6a911bb19ae306e97de3e34d07c70762
Cliff Chen <cliff.chen@rock-chips.com>
2024-06-18 07:29:51 +0000

    [bsp] rockchip: rk3588-64: memory align with 2M bytes
    Change-Id: I185e76c1cd7b5a6a5f9eea96d3f09abbe713d68f
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

--- bsp/rockchip/rk3588-64/build.sh
+++ bsp/rockchip/rk3588-64/build.sh
@@ -11,7 +11,7 @@ usage() {
 }
 
 # TODO: Please plan the memory according to the actual needs of the project.
-# Requiring 1M alignment.
+# Requiring 2M alignment.
 CPU0_MEM_BASE=0x02000000
 CPU1_MEM_BASE=0x00800000
 CPU2_MEM_BASE=0x01000000
@@ -27,7 +27,7 @@ make_rtt() {
     export RTT_SHMEM_BASE=0x07800000
     export RTT_SHMEM_SIZE=0x00400000
     export LINUX_RPMSG_BASE=0x07c00000
-    export LINUX_RPMSG_SIZE=0x00500000
+    export LINUX_RPMSG_SIZE=0x00600000
     export CUR_CPU=$1
     scons -c
     rm -rf $CUR_DIR/gcc_arm.ld $IMAGE/amp.img $IMAGE/rtt$1.elf $IMAGE/rtt$1.bin
