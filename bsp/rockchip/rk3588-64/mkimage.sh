d736519c887e672fa589b6046b45133e19c4e4c8
Cliff Chen <cliff.chen@rock-chips.com>
2024-06-19 03:35:32 +0000

    [bsp] rockchip: rk3588-64: Add its parameter for mkimage.sh
    Change-Id: Idc887eb605235d91bd41762e11c8eed110c6ae5f
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

--- bsp/rockchip/rk3588-64/mkimage.sh
+++ bsp/rockchip/rk3588-64/mkimage.sh
@@ -4,13 +4,17 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 usage() {
-	echo "usage: ./mkimage.sh"
+	echo "usage: ./mkimage.sh [its]"
 }
 
 CUR_DIR=$(pwd)
 TOOLS=$CUR_DIR/../tools
 IMAGE=$(pwd)/Image
 
-$TOOLS/mkimage -f $IMAGE/amp.its -E -p 0x1c00 $IMAGE/amp.img
+if [ ! -n "$1" ] ;then
+    $TOOLS/mkimage -f $IMAGE/smp.its -E -p 0x1c00 $IMAGE/amp.img
+else
+    $TOOLS/mkimage -f $1 -E -p 0x1c00 $IMAGE/amp.img
+fi
 
 echo 'Image: amp.img is ready.'
